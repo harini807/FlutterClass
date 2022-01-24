cf81a53c380458c74092ddb203451f43d8687bc1
harini <76867333+harini807@users.noreply.github.com>
2022-01-24 11:13:37 +0530

    updated

--- harini/task/ebill/lib/main.dart
+++ harini/task/ebill/lib/main.dart
@@ -1,5 +1,3 @@
-
-
 import "package:flutter/material.dart";
 
 import "dart:math";
@@ -21,14 +19,14 @@ class HomeScreenState extends State<HomeScreen> {
   String _ebResult = "";
 
   final TextEditingController _EnterFinalReading = TextEditingController();
-  final TextEditingController _EnterInitialReading = TextEditingController();
 
   bool _switchValue = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("EB Bill"), centerTitle:true,elevation: 0.0),
+        appBar:
+            AppBar(title: Text("EB Bill"), centerTitle: true, elevation: 0.0),
         body: Center(
             child: Container(
                 child: Column(
@@ -36,36 +34,49 @@ class HomeScreenState extends State<HomeScreen> {
             Container(
                 padding: EdgeInsets.all(20.0),
                 child: TextField(
-                  
                   controller: _EnterFinalReading,
-                  decoration: InputDecoration(labelText: "Enter Final Reading",fillColor: Colors.yellow),
+                  decoration: InputDecoration(
+                      labelText: "Enter Final Reading",
+                      fillColor: Colors.yellow),
                   keyboardType: TextInputType.number,
                 )),
-            Container(
-                padding: EdgeInsets.all(20.0),
-                child: TextField(
-                  controller: _EnterInitialReading,
-                  decoration: InputDecoration(labelText: "Enter Initial Reading",fillColor: Colors.red),
-                  keyboardType: TextInputType.number,
-                )),
-            Flexible(fit: FlexFit.loose, child: FlatButton(onPressed: _handleCalculation, child: Text("CALCULATE"), color: Colors.redAccent, textColor: Colors.white, padding: EdgeInsets.only(top: 10.0, bottom: 10.0, left: 24.0, right: 24.0))),
+            Flexible(
+                fit: FlexFit.loose,
+                child: FlatButton(
+                    onPressed: _handleCalculation,
+                    child: Text("CALCULATE"),
+                    color: Colors.redAccent,
+                    textColor: Colors.white,
+                    padding: EdgeInsets.only(
+                        top: 10.0, bottom: 10.0, left: 24.0, right: 24.0))),
             ebResultsWidget(_ebResult)
           ],
         ))));
   }
 
   void _handleCalculation() {
-    
-    
+    double sum = 0.0;
+    var a;
+    var b;
+    var c;
 
-    double A = 0.0;
-    int c = 0;
     int f = int.parse(_EnterFinalReading.text);
-    int i = int.parse(_EnterInitialReading.text);
 
-    A = f-i/24;
+    if (f > 500) {
+      a = (f - 500) * 6.6;
+      b = 300 * 4.6;
+      c = 100 * 3.5;
+      sum = a + b + c + 50;
+    } else if (f > 200) {
+      a = (f - 200) * 3;
+      b = 100 * 2;
+      sum = a + b + c + 30;
+    } else if (f > 100) {
+      a = (f - 100) * 1.5;
+      sum = a + 20;
+    }
 
-    _ebResult = A.toStringAsFixed(2);
+    _ebResult = sum.toStringAsFixed(2);
 
     setState(() {});
   }
@@ -73,17 +84,21 @@ class HomeScreenState extends State<HomeScreen> {
   Widget ebResultsWidget(ebResult) {
     bool canShow = false;
     String _ebResult = ebResult;
-
-    if (_ebResult.length > 0) {
-      canShow = true;
-    }
+  
     return Container(
         margin: EdgeInsets.only(top: 40.0),
-        child: canShow
-            ? Column(children: [
-                Text("EB BILL", style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
-                Container(child: Text(_ebResult, style: TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold)))
-              ])
-            : Container());
+        child: Column(children: [
+                Text("EB BILL",
+                    style:
+                        TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
+                Container(
+                    child: Text(_ebResult,
+                        style: TextStyle(
+                            fontSize: 50.0, fontWeight: FontWeight.bold)))
+              ]));
   }
 }
+
+
+  
+   
